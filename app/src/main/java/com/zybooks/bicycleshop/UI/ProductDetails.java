104b1851434f17dce763918215f13061e8b98b26
clohrma <104752286+clohrma@users.noreply.github.com>
2022-12-22 00:21:20 -0500

    Part 1 complete

--- app/src/main/java/com/zybooks/bicycleshop/UI/ProductDetails.java
+++ app/src/main/java/com/zybooks/bicycleshop/UI/ProductDetails.java
@@ -1,25 +1,23 @@
 package com.zybooks.bicycleshop.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zybooks.bicycleshop.R;
 
-public class ProductList extends AppCompatActivity {
+public class ProductDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_product_details);
+        FloatingActionButton fab = findViewById(R.id.productDetailsFAB);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(ProductList.this, ProductDetails.class );
+                Intent intent = new Intent(ProductDetails.this, PartDetails.class );
                 startActivity(intent);
             }
         });
