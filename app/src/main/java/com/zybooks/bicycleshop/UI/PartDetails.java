6ed94fd576d565beed958f3213c20e57e34fc2cc
clohrma <104752286+clohrma@users.noreply.github.com>
2022-12-22 00:21:20 -0500

    Part 1 complete

--- app/src/main/java/com/zybooks/bicycleshop/UI/PartDetails.java
+++ app/src/main/java/com/zybooks/bicycleshop/UI/PartDetails.java
@@ -9,17 +9,17 @@ import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zybooks.bicycleshop.R;
 
-public class ProductList extends AppCompatActivity {
+public class PartDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_part_details);
+        FloatingActionButton fab = findViewById(R.id.partDetailsFAB);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(ProductList.this, ProductDetails.class );
+                Intent intent = new Intent(PartDetails.this, ProductDetails.class );
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/zybooks/bicycleshop/UI/ProductDetails.java
+++ app/src/main/java/com/zybooks/bicycleshop/UI/ProductDetails.java
@@ -9,17 +9,17 @@ import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zybooks.bicycleshop.R;
 
-public class ProductList extends AppCompatActivity {
+public class ProductDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_product_details);
+        FloatingActionButton fab = findViewById(R.id.productDetailsFAB);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(ProductList.this, ProductDetails.class );
+                Intent intent = new Intent(ProductDetails.this, PartDetails.class );
                 startActivity(intent);
             }
         });
